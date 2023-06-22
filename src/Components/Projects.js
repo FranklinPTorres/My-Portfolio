fb93a9090645f391ac69ba356761c8b3997c98a6
FranklinPTorres <105072367+FranklinPTorres@users.noreply.github.com>
2023-06-22 22:13:50 +0100

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import pictures from './images/pro1.png'
+import pictures  from './images/pro1.png'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
